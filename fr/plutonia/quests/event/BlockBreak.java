cced04d6e0832dc1d3285b3a678a21e77c75659b
ayymvrder <38377158+ayymvrder@users.noreply.github.com>
2018-05-05 19:11:14 +0200

    Update BlockBreak.java

--- fr/plutonia/quests/event/BlockBreak.java
+++ fr/plutonia/quests/event/BlockBreak.java
@@ -1,5 +1,9 @@
 package fr.plutonia.quests.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,14 +16,22 @@ import fr.plutonia.quests.Type;
 
 public class BlockBreak implements Listener {
 	
+	private List<Material> allowed = new ArrayList<Material>() {
+		private static final long serialVersionUID = 1L;
+	{
+		add(Material.STONE);
+		add(Material.GRASS);
+		add(Material.OBSIDIAN);
+	}};
+	
 	@EventHandler
 	public void onBlockBreak(BlockBreakEvent event) {
 		Player player = event.getPlayer();
 		QPlayer qplayer = Core.getQPlayer(player);
 		Quest current = qplayer.getCurrentQuest();
 		
-		if(current.getId() == Type.BREAK_BLOCK.getId()) {
-			
+		if(current.getId() == Type.BREAK_BLOCK.getId() && allowed.contains(event.getBlock().getType())) {
+			current.setProgression(current.getProgression() + 1);
 		}
 	}
 
