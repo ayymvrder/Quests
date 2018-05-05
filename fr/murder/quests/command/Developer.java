95efa4b8e2f7d7921b9b15222f8771653b000e02
ayymvrder <38377158+ayymvrder@users.noreply.github.com>
2018-05-05 16:43:46 +0200

    Create Developer.java

---
+++ fr/murder/quests/command/Developer.java
@@ -0,0 +1,38 @@
+package fr.murder.quests.command;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import fr.murder.quests.Core;
+
+public class Developer implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(! sender.hasPermission("quests.developer")) {
+			sender.sendMessage(ChatColor.RED + "You don't have the permission to execute this command");
+			return true;
+		}
+		
+		if(args.length != 1) {
+			sender.sendMessage(ChatColor.RED + "Usage: /developer <command>");
+			return true;
+		}
+		
+		if(args[0].equalsIgnoreCase("nextday")) {
+			Core.getQPlayer((Player) sender).setDate(null);
+			sender.sendMessage(ChatColor.GREEN + "You are now on a new day");
+		}
+		
+		if(args[0].equalsIgnoreCase("succeedquest")) {
+			Core.getQPlayer((Player) sender).setDate(null);
+			sender.sendMessage(ChatColor.GREEN + "You have now finished your daily quest");
+		}
+		
+		return false;
+	}
+
+}
